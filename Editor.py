285f251b07a192602f9a28a4cc72e2d0d72a8a03
= <=>
2019-12-03 07:58:40 +0530

    Create Editor.py
    1st commit (adding menu )

---
+++ Editor.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font, colorchooser, filedialog, messagebox
+import os
+main_application = tk.Tk()
+main_application.geometry('1200x800')
+main_application.title('Vpad text editor')
+
+
+################################## main menu #############################
+#--------------------------------- end of main menu ----------------------
+
+
+
+
+
+################################## main menu #############################
+#--------------------------------- end of main menu ----------------------
+
+
+
+
+
+# ################################## main menu #############################
+#--------------------------------- end of main menu ----------------------
+
+
+
+# ################################## main menu #############################
+#--------------------------------- end of main menu ----------------------
+
+
+
+################################## main menu #############################
+#--------------------------------- end of main menu ----------------------
+main_application.mainloop()
